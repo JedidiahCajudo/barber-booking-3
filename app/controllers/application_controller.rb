7325947594fe6809f950b30d8aadd1bdc356714f
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-22 18:36:17 +0900

    sign out link_to changed to button_to and actually works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Redirect after sign out
+  def after_sign_out_path_for(resource_or_scope)
+    root_path  # or specify the path you want to redirect to, e.g., login_path
+  end
 end
