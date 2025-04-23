5fb55f2f986d853d364fcd41e80e4d4426719e1f
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-23 14:02:23 +0900

    reinstalled bin/importmap and put a small placeholder for #calendar in the SASS

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,3 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/ujs", to: "rails-ujs.js"
-pin "fullcalendar", to: "https://cdn.jsdelivr.net/npm/fullcalendar@5.11.2/main.min.js"
