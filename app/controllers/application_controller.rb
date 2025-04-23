b5d9dd396738b7eb85628c114471b347b265cac3
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-23 18:22:51 +0900

    added pronouns and nickname to sign up parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path  # or specify the path you want to redirect to, e.g., login_path
   end
+
+  # Permit additional fields for Devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :pronouns])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :pronouns])
+  end
 end
