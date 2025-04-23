77f0a24908e143449167f2536338a9cb5ac0f08b
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-23 14:58:13 +0900

    gave up on FullCalendar and stimulus for now

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,1 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// application.js, after removing Stimulus and controllers
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/ujs", to: "rails-ujs.js"
