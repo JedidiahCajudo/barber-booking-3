d9ab46c7a0f79607a5bef2e2e8f660dabe3408ec
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-25 15:48:00 +0900

    wrote 10 seeds for hairstyles. not seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+Hairstyle.destroy_all  # This clears existing records
+
+Hairstyle.create([
+  { name: 'Buzz Cut', image: 'buzz_cut.jpg' },
+  { name: 'Pompadour', image: 'pompadour.jpg' },
+  { name: 'Crew Cut', image: 'crew_cut.jpg' },
+  { name: 'Modern Mullet', image: 'modern_mullet.jpg' },
+  { name: 'Quiff', image: 'quiff.jpg' },
+  { name: 'Side Part', image: 'side_part.jpg' },
+  { name: 'French Crop', image: 'french_crop.jpg' },
+  { name: 'Caesar Cut', image: 'caesar_cut.jpg' },
+  { name: 'Comb Over', image: 'comb_over.jpg' },
+  { name: 'Bro Flow', image: 'bro_flow.jpg' },
+])
